3ba785c9dca381a6ce788420bb75f394b4263801
kcli2412 <kcli2412@gmail.com>
2018-01-03 13:33:22 +0800

    setImage

--- app/src/main/java/com/example/student/dd2018010301/MainActivity.java
+++ app/src/main/java/com/example/student/dd2018010301/MainActivity.java
@@ -2,12 +2,21 @@ package com.example.student.dd2018010301;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    ImageView img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void click1(View v)
+    {
+        img = (ImageView) findViewById(R.id.imageView2);
+        img.setImageResource(R.drawable.flower);
     }
 }
